427f36d0d63aa4f314796937ff1911f553713bf5
illyakurochkin <illya.kurochkin@gmail.com>
2019-06-02 16:31:55 +0300

    Updates

---
+++ precache-manifest.81cb6bde0466901a06ed164762423011.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f646df926f76f317ab3694ae109f75af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffb220f7ff38c83d340f",
+    "url": "/static/js/2.b6eafef6.chunk.js"
+  },
+  {
+    "revision": "02dd3a80f5d5f234c282",
+    "url": "/static/js/main.9d1017fc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
